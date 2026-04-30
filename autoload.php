e1eabf79eded7e3d6651c51ccfec6871233fb8b7
Makinde Ayooluwa <makindeayooluwa604@gmail.com>
2026-04-30 13:14:35 +0100

    shopping-app-backend created - AyExpertDev

---
+++ autoload.php
@@ -0,0 +1,9 @@
+<?php
+
+spl_autoload_register("autoload");
+
+function autoload($class_name){
+    $path = "classes/";
+    $extension = ".class.php";
+    $fullPath = $path . $class_name . $extension
+}
---
+++ classes/Database.class.php
@@ -0,0 +1,24 @@
+<?php
+
+class Database
+{
+    public $pdo;
+    public $dbname = 'shopping-app';
+    public $username = "root";
+    public $password = "";
+    public function __construct($host)
+    {
+        $this->pdo = new PDO("mysql:host=$host;dbname=$this->dbname", $this->username, $this->password);
+    }
+    public function connect()
+    {
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $this->pdo;
+    }
+    public function queryDb($query)
+    {
+        $this->pdo->query($query);
+        return "Db queried successfully";
+    }
+}
