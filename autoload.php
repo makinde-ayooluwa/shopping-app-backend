d8342a13b24548c57a7537267b31882ac76f439c
Makinde Ayooluwa <makindeayooluwa604@gmail.com>
2026-04-30 13:14:46 +0100

    shopping-app-backend created - AyExpertDev

--- autoload.php
+++ autoload.php
@@ -5,5 +5,5 @@ spl_autoload_register("autoload");
 function autoload($class_name){
     $path = "classes/";
     $extension = ".class.php";
-    $fullPath = $path . $class_name . $extension
+    $fullPath = $path . $class_name . $extension;
 }
